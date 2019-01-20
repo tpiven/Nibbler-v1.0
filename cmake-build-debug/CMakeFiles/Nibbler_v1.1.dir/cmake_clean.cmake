10e135b311019f321ce19bb08653d3b8970cb346
tpiven <tatiynapiven@gmail.com>
2019-01-20 16:54:55 +0200

    q
    Signed-off-by: tpiven <tatiynapiven@gmail.com>

---
+++ cmake-build-debug/CMakeFiles/Nibbler_v1.1.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Nibbler_v1.1.dir/main.cpp.o"
+  "CMakeFiles/Nibbler_v1.1.dir/Game_Obj.cpp.o"
+  "CMakeFiles/Nibbler_v1.1.dir/Logic.cpp.o"
+  "CMakeFiles/Nibbler_v1.1.dir/Mmap.cpp.o"
+  "CMakeFiles/Nibbler_v1.1.dir/AView.cpp.o"
+  "CMakeFiles/Nibbler_v1.1.dir/Food.cpp.o"
+  "CMakeFiles/Nibbler_v1.1.dir/SFML_lib.cpp.o"
+  "CMakeFiles/Nibbler_v1.1.dir/SDL_lib.cpp.o"
+  "Nibbler_v1.1.pdb"
+  "Nibbler_v1.1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Nibbler_v1.1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
